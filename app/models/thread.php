6fb7e44dd4950f6983c473b390a986bebd6b3313
Chey Luna <rachel.luna@klab.com>
2014-09-16 12:09:05 +0800

    Added validation rule and create() method in models/thread.php.

--- app/models/thread.php
+++ app/models/thread.php
@@ -1,6 +1,14 @@
 <?php
 class Thread extends AppModel
 {
+    public $validation = array(
+        'title' => array(
+            'length' => array(
+                'validate_between', 1, 30,
+            ),
+        ),
+    );
+
     public static function get($id)
     {
         $db = DB::conn();
@@ -53,5 +61,27 @@ class Thread extends AppModel
         'INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()',
         array($this->id, $comment->username, $comment->body)
         );
+    }
+
+    public function create(Comment $comment)
+    {
+        $this->validate();
+        $comment->validate();
+        if($this->hasError() || $comment->hasError()) {
+            throw new ValidationException('invalid thread or comment');
+        }
+
+        $db = DB::conn();
+        $db->begin();
+        $params = array(
+            'title' => $this->title,
+        );
+        $db->insert('thread', $params);
+        $this->id = $db->lastInsertId();
+
+        // write first comment at the same time
+        $this->write($comment);
+
+        $db->commit();
     }
 }
