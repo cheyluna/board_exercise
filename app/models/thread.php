1399368ab781147dd0696b4fcfdb1cdb91d041ee
Chey Luna <rachel.luna@klab.com>
2014-09-16 09:42:27 +0800

    Added get() method in models/thread.php.

--- app/models/thread.php
+++ app/models/thread.php
@@ -1,6 +1,18 @@
 <?php
 class Thread extends AppModel
 {
+    public static function get($id)
+    {
+        $db = DB::conn();
+        $row = $db->rows('SELECT * FROM thread WHERE id = ?', array($id));
+
+        if (!$row) {
+            throw new RecordNotFoundException('no record found');
+        }
+
+        return new self($row);
+    }
+
     public static function getAll()
     {
         $threads = array();
