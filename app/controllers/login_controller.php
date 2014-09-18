20ad8f1d48ef37a83c325feae2e27373abe8c04c
Chey Luna <rachel.luna@klab.com>
2014-09-18 11:14:33 +0800

    Added checkUserLogin() method in login_controller.php.

--- app/controllers/login_controller.php
+++ app/controllers/login_controller.php
@@ -6,4 +6,24 @@ class LoginController extends AppController
         // TODO: Show login page
         $this->set(get_defined_vars());
     }
+
+    public function checkUserLogin()
+    {
+        $unvalidated_user = new User;
+        $page = Param::get('page_next');
+
+        switch ($page) {
+            case 'thread/index':
+                $unvalidated_user->username = Param::get('username');
+                $unvalidated_user->password = Param::get('password');
+                $validated_user = Login::checkValidUser($unvalidated_user);
+                break;
+            default:
+                throw new NotFoundException("{$page} is not found");
+                break;
+        }
+
+        $this->set(get_defined_vars());
+        $this->render($page);
+    }
 }
