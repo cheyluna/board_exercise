f036989efc1c0bb70d186b3e62eebd54f527e10e
Chey Luna <rachel.luna@klab.com>
2014-09-18 11:44:15 +0800

    Added validation rule in models/user.php.

--- app/models/user.php
+++ app/models/user.php
@@ -1,5 +1,17 @@
 <?php
 class User extends AppModel
 {
+    public $validation = array(
+        'username' => array(
+            'length' => array(
+                'validate_between', 1, 20,
+            ),
+        ),
 
+        'body' => array(
+            'length' => array(
+                'validate_between', 1, 20,
+            ),
+        ),
+    );
 }
