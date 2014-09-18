7d9377caf7349617a2c511b2748e8ffe038fcf23
Chey Luna <rachel.luna@klab.com>
2014-09-18 11:16:49 +0800

    Added call to checkUserLogin() method, changed name attributes for Username and Password, and changed value of page_next hidden input in views/login/index.php.

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,11 +1,11 @@
 <h1>Login</h1>
 
-<form class="well" method="post" action="<?php eh(url('')) ?>">
+<form class="well" method="post" action="<?php eh(url('login/checkUserLogin')) ?>">
     <label>Username</label>
-    <input type="text" class="span2" name="title" value="<?php eh(Param::get('title')) ?>">
-    <label>Password</label>
     <input type="text" class="span2" name="username" value="<?php eh(Param::get('username')) ?>">
+    <label>Password</label>
+    <input type="text" class="span2" name="password" value="<?php eh(Param::get('password')) ?>">
     <br />
-    <input type="hidden" name="page_next" value="create_end">
+    <input type="hidden" name="page_next" value="thread/index">
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
