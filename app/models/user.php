e08eed74d77ffa0f1c3284dec52ced8a81cce410
Chey Luna <rachel.luna@klab.com>
2014-09-24 11:08:00 +0800

    Added register() method and user registration validation rules in models/user.php.

--- app/models/user.php
+++ app/models/user.php
@@ -9,6 +9,21 @@ class User extends AppModel
     const MAX_PASSWORD_LENGTH = 20;
 
     public $validation = array(
+        'name' => array (
+            'format' => array(
+                'letters_only',
+            ),
+        ),
+
+        'email' => array(
+            'format' => array(
+                'email_valid',
+            ),
+            'availability' => array(
+                'is_not_available',
+            ),
+        ),
+
         'username' => array(
             'length' => array(
                 'validate_between', self::MIN_USER_LENGTH, self::MAX_USER_LENGTH,
@@ -20,7 +35,46 @@ class User extends AppModel
                 'validate_between', self::MIN_PASSWORD_LENGTH, self::MAX_PASSWORD_LENGTH,
             ),
         ),
+
+        'confirm_password' => array(
+            'match' => array(
+                'match_password',
+            ),
+        ),
     );
+
+    /**
+    * Add a new user
+    * @param $user
+    */
+    public function register(User $user)
+    {
+        $this->validation['name']['format'][] = $this->name;
+        $this->validation['email']['format'][] = $this->email;
+        $this->email_exists = $this->checkAvailability($this->email, $check='email');
+        $this->validation['email']['availability'][] = $this->email_exists;
+        $this->validation['email']['availability'][] = true;
+        $this->username_exists = $this->checkAvailability($this->username, $check='uname');
+        $this->validation['username']['availability'][] = $this->username_exists;
+        $this->validation['username']['availability'][] = true;
+        $this->validation['confirm_password']['match'][] = $this->password;
+        $this->validation['confirm_password']['match'][] = $this->confirm_password;
+        $this->validate();
+
+        if($this->hasError()) {
+            throw new ValidationException("invalid inputs");
+        } else {
+            $params = array(
+            'name' => $this->name,
+            'email' => $this->email,
+            'username' => $this->username,
+            'password' => sha1($this->password)
+            );
+        }
+
+        $db = DB::conn();
+        $db->insert('user', $params);
+    }
 
     public function checkValidUser(User $user)
     {
