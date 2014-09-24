720c706a97a86710737332281b193620d1fdb4c3
Chey Luna <rachel.luna@klab.com>
2014-09-24 11:52:58 +0800

    Added views/user/register_ok.php.

---
+++ app/views/user/register_ok.php
@@ -0,0 +1,7 @@
+<h2>Congratulations!</h2>
+<p class="alert alert-success">
+Your registration was successful. <br/>
+You may now log in to your account. <br/>
+</p>
+
+<a class='btn btn-primary btn-medium' href="<?php eh(url('user/login'))?>">Log In</a>
