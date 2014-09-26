3c0f171d72f59af3f92214a5612bfda156d9cfa7
Chey Luna <rachel.luna@klab.com>
2014-09-26 11:50:31 +0800

    Fixed indetations and magic numbers in lib/pagination.php.

--- lib/pagination.php
+++ lib/pagination.php
@@ -3,6 +3,7 @@ class Pagination
 {
     const MAX_ROWS = 5;
     const PAGINATION_LIMIT = 4;
+    const FIRST_PAGE = 1;
 
     public static $pagination_for;
 
@@ -19,6 +20,7 @@ class Pagination
         if ($total_rows <= self::MAX_ROWS) {
             return 1;
         }
+
         return ceil($total_rows/self::MAX_ROWS);
     }
 
@@ -28,6 +30,7 @@ class Pagination
         if ($page > 1) {
             return self::$current_page = preg_replace('#[^0-9]#', '', $page);
         }
+
         return self::$current_page;
     }
 
@@ -47,16 +50,20 @@ class Pagination
         $right_pagenum = $current_page + self::PAGINATION_LIMIT;
 
         // Links will be generated if there are more than 1 page.
-        if ($last != 1) {
+        if ($last != self::FIRST_PAGE) {
             $page_links .= self::createFirstLink();
             $page_links .= self::createPreviousLink($current_page, $previous);
+
             for ($i = $left_pagenum; $i < $current_page; $i++) {
                 $page_links .= self::createLeftLinks($i);
             }
+
             $page_links .= '' . $current_page . ' &nbsp; ';
+
             for ($i = $next; $i <= $last; $i++) {
                 $page_links .= self::createRightLinks($i,$right_pagenum);
             }
+
             $page_links .= self::createNextLink($current_page, $last, $next);
             $page_links .= self::createLastLink($last);
         }
@@ -68,7 +75,7 @@ class Pagination
     {
          switch (self::$pagination_for) {
              case 'comment':
-                 return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page=1'><<</a> &nbsp; &nbsp;";
+                return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page=1'><<</a> &nbsp; &nbsp;";
              default:
                 return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page=1'><<</a> &nbsp; &nbsp;";
         }
@@ -76,74 +83,66 @@ class Pagination
 
     public static function createPreviousLink($page_num, $previous)
     {
-        if($page_num > 1)
-        {
+        if ($page_num > 1) {
             switch (self::$pagination_for) {
                 case 'comment':
-                return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$previous}'><</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$previous}'><</a> &nbsp; &nbsp;";
                 case 'search':
-                return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$previous}'><</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$previous}'><</a> &nbsp; &nbsp;";
                 case 'my_threads':
-                return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$previous}'><</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$previous}'><</a> &nbsp; &nbsp;";
                 default:
-                return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$previous}'><</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$previous}'><</a> &nbsp; &nbsp;";
             }
-        } else
-        return null;
+        }
     }
 
     public static function createLeftLinks($page_num)
     {
-        if($page_num > 0)
-        {
+        if ($page_num > 0) {
             switch (self::$pagination_for) {
                 case 'comment':
-                return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
                 case 'search':
-                return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
                 case 'my_threads':
-                return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
                 default:
-                return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
             }
-        } else
-        return null;
+        }
     }
 
     public static function createRightLinks($page_num, $right_pagenum)
     {
-        if($page_num <= $right_pagenum)
-        {
+        if ($page_num <= $right_pagenum) {
             switch (self::$pagination_for) {
                 case 'comment':
-                return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
                 case 'search':
-                return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
                 case 'my_threads':
-                return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
                 default:
-                return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$page_num}'>{$page_num}</a> &nbsp; &nbsp;";
             }
-        } else
-        return null;
+        }
     }
 
     public static function createNextLink($page_num, $last, $next)
     {
-        if($page_num != $last)
-        {
+        if ($page_num != $last) {
             switch (self::$pagination_for) {
                 case 'comment':
-                return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$next}'>></a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?thread_id=" . Param::get('thread_id') . "&page={$next}'>></a> &nbsp; &nbsp;";
                 case 'search':
-                return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$next}'>></a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?keyword=" . Param::get('keyword') . "&search_by=" . Param::get('search_by') . "&page={$next}'>></a> &nbsp; &nbsp;";
                 case 'my_threads':
-                return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$next}'>></a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?user_id=" . Param::get('user_id') . "&page={$next}'>></a> &nbsp; &nbsp;";
                 default:
-                return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$next}'>></a> &nbsp; &nbsp;";
+                    return "<a class='btn btn-small' href='?sort_by=" . Param::get('sort_by') . "&sort_order=" . Param::get('sort_order') . "&page={$next}'>></a> &nbsp; &nbsp;";
             }
-        } else
-        return null;
+        }
     }
 
     public static function createLastLink($last_page_num)
